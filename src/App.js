4cc203688c3044ad9cd0689d6466a1af86e8366a
maher <maherramadan2012@hotmail.com>
2023-03-22 08:49:10 -0600

    Fix linters error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Calculator from './components/Calculator';
+// eslint-disable-next-line import/extensions
+import Calculator from './components/Calculator.js';
 import './App.css';
 
 const App = () => (
