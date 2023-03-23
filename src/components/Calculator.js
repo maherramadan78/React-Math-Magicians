8e663b76c4c9fa2b3fc67f08b5a05404618b144c
maher <maherramadan2012@hotmail.com>
2023-03-23 15:13:19 -0600

    Update  App.js and calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,47 +1,30 @@
-import React, { useState } from 'react';
-import calculate from './logic/calculate';
+import { useState } from 'react';
+import Btn from './button';
+import Output from './output';
 
 function Calculator() {
-  const [result, setResult] = useState({
-    total: null,
+  const btnValues = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  const [value, setValue] = useState('0');
+  const initialCalc = {
+    total: '0',
     next: null,
     operation: null,
-  });
-
-  const handleClick = (buttonName) => {
-    setResult(calculate(result, buttonName));
   };
-  // Calculator html body with the handle click function when we click button (onClick)
-  return (
-    <div className="calculatrice">
-      <div className="display-screen-container">
-        <input className="display-screen" type="text" value={result.next || result.total || '0'} />
-      </div>
-      <div className="all-buttons">
-        <div className="buttons">
+  const [totalObj, setTotal] = useState(initialCalc);
 
-          <button type="button" className="button" onClick={() => handleClick('AC')}>AC</button>
-          <button type="button" className="button" onClick={() => handleClick('+/-')}>+/-</button>
-          <button type="button" className="button" onClick={() => handleClick('%')}>%</button>
-          <button type="button" className="button" onClick={() => handleClick('7')}>7</button>
-          <button type="button" className="button" onClick={() => handleClick('8')}>8</button>
-          <button type="button" className="button" onClick={() => handleClick('9')}>9</button>
-          <button type="button" className="button" onClick={() => handleClick('4')}>4</button>
-          <button type="button" className="button" onClick={() => handleClick('5')}>5</button>
-          <button type="button" className="button" onClick={() => handleClick('6')}>6</button>
-          <button type="button" className="button" onClick={() => handleClick('1')}>1</button>
-          <button type="button" className="button" onClick={() => handleClick('2')}>2</button>
-          <button type="button" className="button" onClick={() => handleClick('3')}>3</button>
-          <button type="button" className="button zero" onClick={() => handleClick('0')}>0</button>
-          <button type="button" className="button" onClick={() => handleClick('.')}>.</button>
-        </div>
-        <div className="operations">
-          <button type="button" className="ope divide" onClick={() => handleClick('÷')}>÷</button>
-          <button type="button" className="ope por" onClick={() => handleClick('x')}>x</button>
-          <button type="button" className="ope minus" onClick={() => handleClick('-')}>-</button>
-          <button type="button" className="ope sum" onClick={() => handleClick('+')}>+</button>
-          <button type="button" className="equal" onClick={() => handleClick('=')}>=</button>
-        </div>
+  return (
+    <div className="calculator">
+      <Output value={value} totalObj={totalObj} />
+      <div className="calculator-btn">
+        {btnValues.map((btn) => (
+          <Btn
+            key={btn}
+            content={btn}
+            setValue={setValue}
+            setTotal={setTotal}
+            totalObj={totalObj}
+          />
+        ))}
       </div>
     </div>
   );
