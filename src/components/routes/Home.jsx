64f9e5e4e7c46ace915c2d4409d7f79aaed6cba3
maher <maherramadan2012@hotmail.com>
2023-03-29 13:01:09 -0600

    Update Home .jsx

--- src/components/routes/Home.jsx
+++ src/components/routes/Home.jsx
@@ -4,16 +4,18 @@ const Home = () => (
   <div className={styles.homeDiv}>
     <h2 className={styles.h2Home}>Welcome to our page</h2>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Maiores illum possimus sed. Nihil modi quos, necessitatibus
-      minus cumque iste itaque blanditiis. Possimus dolorum, molestiae
-      nesciunt qui vero fugit ratione modi.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Fuga cupiditate, nemo perferendis sint consequatur odit hic
-      repellendus soluta sequi inventore doloribus, vitae unde possimus?
-      Ipsam nam reiciendis nobis debitis atque
+      They are among the simpler interactive software tools, and, as such, they provide operations
+      for the user to select one at a time.
+      They can be used to perform any process that consists
+      of a sequence of steps each of which applies.
+      one of these operations, and have no purpose other
+      than these processes, because the operations are the sole, or at least
+      the primary, features of the calculator, rather than being secondary features
+      that support other functionality that is not normally
+      known simply as calculation. As a calculator, rather than a computer.
+      they usually have a small set of relatively simple operations
+      perform short processes that are not compute intensive and do not accept large amounts
+      of input data or produce many results.
     </p>
   </div>
 );
