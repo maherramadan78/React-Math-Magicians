4c9f6ad87b809dfc2e9b77cf4c255f0d40b1f237
maher <maherramadan2012@hotmail.com>
2023-03-29 12:31:56 -0600

    Create full website

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
-import Calculator from './components/Calculator';
-import ApiCall from './components/ApiCalls';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="calculator-holder">
-      <Calculator />
-      <ApiCall />
-    </div>
-  );
-}
+import Layout from './components/Layout';
+import Home from './components/routes/Home';
+import CalculatorPage from './components/routes/CalculatorPage';
+import Quote from './components/routes/Quote';
+import NotMatch from './components/routes/NotMatch';
+import './components/styles/App.css';
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="Calculator" element={<CalculatorPage />} />
+      <Route path="Quote" element={<Quote />} />
+      <Route path="*" element={<NotMatch />} />
+    </Route>
+  </Routes>
+);
 
 export default App;
